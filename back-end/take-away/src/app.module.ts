ab9fc8ba6457adc59cf59bfa4546525588a82cc5
Rodin <rodinwitman9@gmail.com>
2025-05-15 22:00:58 +0300

    Configuration TypeOrmModule part 2

--- back-end/take-away/src/app.module.ts
+++ back-end/take-away/src/app.module.ts
@@ -2,17 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './event.controller';
+import { Event } from './event.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  //install TypeOrm : npm i --save @nestjs/typeorm typeorm mysql
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
     host: '127.0.0.1',
     port: 3306,
     username: 'root',
     password: 'example',
-    database: 'nest-events'
-  })],
+    database: 'nest-events',
+    entities: [Event],
+    synchronize: true
+  }),
+TypeOrmModule.forFeature([Event])],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+//
