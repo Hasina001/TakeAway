a8e79ac7bfa0a18db38389011060cb89de0b03f9
Rodin <rodinwitman9@gmail.com>
2025-05-15 11:27:35 +0300

    Configuration TypeOrmModule

--- back-end/take-away/src/app.module.ts
+++ back-end/take-away/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsController } from './event.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'example',
+    database: 'nest-events'
+  })],
+  controllers: [AppController, EventsController],
   providers: [AppService],
 })
 export class AppModule {}
