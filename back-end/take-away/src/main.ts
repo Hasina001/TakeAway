8289b278dff2ecd606dc286b579019926df6815e
Rodin <rodinwitman9@gmail.com>
2025-06-17 21:59:05 +0300

    configuration des paramettre de blog

--- back-end/take-away/src/main.ts
+++ back-end/take-away/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); 
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // ignore les champs non attendus
+    forbidNonWhitelisted: true, // erreur si champ inattendu
+    transform: true, // transforme les types automatiquement
+    })
+  ); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
